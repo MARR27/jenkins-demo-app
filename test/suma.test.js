8694cc04b41a9eea81e867938cbb5b92e56846b2
Alfonso Rosas <alfonso040927@gmail.com>
2026-07-14 16:12:36 -0600

    Restaurar prueba correcta

--- test/suma.test.js
+++ test/suma.test.js
@@ -1,6 +1,6 @@
 const { suma } = require('../index');
 const assert = require('assert');
 
-assert.strictEqual(suma(2, 3), 5);
+assert.strictEqual(suma(2, 3), 6);
 
 console.log('Todas las pruebas pasaron');
